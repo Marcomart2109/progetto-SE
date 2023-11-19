87c74d803726e587ab4253ccffb3a09f2677bfda
Marco Martelli <marcomartelli912@gmail.com>
2023-11-19 12:59:16 +0100

    Project Initialize

---
+++ src/main/java/it/unisa/diem/se/group7/seproject/Controller/MainController.java
@@ -0,0 +1,14 @@
+package it.unisa.diem.se.group7.seproject.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module it.unisa.diem.se.group7.seproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.unisa.diem.se.group7.seproject to javafx.fxml;
+    exports it.unisa.diem.se.group7.seproject;
+    exports it.unisa.diem.se.group7.seproject.Controller;
+    opens it.unisa.diem.se.group7.seproject.Controller to javafx.fxml;
+}
