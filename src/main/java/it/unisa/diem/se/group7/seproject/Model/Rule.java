f3ec408cd331455eaae7d5f1b3c093b81f849b9a
Marco Martelli <marcomartelli912@gmail.com>
2023-11-22 15:50:06 +0100

    first UI view and update controller code

--- src/main/java/it/unisa/diem/se/group7/seproject/Model/Rule.java
+++ src/main/java/it/unisa/diem/se/group7/seproject/Model/Rule.java
@@ -1,7 +1,20 @@
 package it.unisa.diem.se.group7.seproject.Model;
 
 public class Rule {
+
+    String name;
     Action action;
     Trigger trigger;
 
+    public Rule(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
