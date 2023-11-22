18096958b928c991d45ebe854fe9564e80f625ce
berto2001 <133567198+berto2001@users.noreply.github.com>
2023-11-22 17:07:08 +0100

    In the class, include a property to store the message specified by the users

--- src/main/java/it/unisa/diem/se/group7/seproject/Model/ShowDialogBoxAction.java
+++ src/main/java/it/unisa/diem/se/group7/seproject/Model/ShowDialogBoxAction.java
@@ -3,7 +3,17 @@ package it.unisa.diem.se.group7.seproject.Model;
 public class ShowDialogBoxAction implements Action{
     private String message;
 
+
     public ShowDialogBoxAction(String message) {
         this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public void execute() {
+
     }
 }
