2a258a9b0d5de7a1fd60e1674425546437e6879f
berto2001 <133567198+berto2001@users.noreply.github.com>
2023-11-22 15:52:04 +0100

    Create a Java class which implements interface class 'Action' named 'ShowDialogBoxAction'

---
+++ src/main/java/it/unisa/diem/se/group7/seproject/Model/ShowDialogBoxAction.java
@@ -0,0 +1,9 @@
+package it.unisa.diem.se.group7.seproject.Model;
+
+public class ShowDialogBoxAction implements Action{
+    private String message;
+
+    public ShowDialogBoxAction(String message) {
+        this.message = message;
+    }
+}
