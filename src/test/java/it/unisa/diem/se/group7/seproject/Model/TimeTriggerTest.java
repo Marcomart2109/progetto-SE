930e4239677799b860d4ea0bc26c8af2d9008d0c
Marco Martelli <marcomartelli912@gmail.com>
2023-11-23 17:01:03 +0100

    fixed TimeTriggerTest

--- src/test/java/it/unisa/diem/se/group7/seproject/Model/TimeTriggerTest.java
+++ src/test/java/it/unisa/diem/se/group7/seproject/Model/TimeTriggerTest.java
@@ -11,13 +11,12 @@ class TimeTriggerTest {
     @Test
     public void testEvaluateFalse() {
 
-        LocalTime currentTime = LocalTime.now();
-
+        LocalTime currentTime = LocalTime.now().plusMinutes(10);
         // Ottieni solo l'ora e i minuti
         int currentHour = currentTime.getHour();
         int currentMinute = currentTime.getMinute();
         // Imposta un orario di attivazione futuro
-        TimeTrigger timeTrigger = new TimeTrigger(currentHour, currentMinute+5);
+        TimeTrigger timeTrigger = new TimeTrigger(currentHour, currentMinute);
 
 
         // Valuta il trigger e verifica che sia attivo
