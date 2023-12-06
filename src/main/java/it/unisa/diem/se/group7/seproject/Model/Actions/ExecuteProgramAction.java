7d2d97d6264b03f781e6e5b5e053123ead0db1c9
Marco Martelli <marcomartelli912@gmail.com>
2023-12-06 11:37:20 +0100

    Fixed error for infinite wait if the program had no execution permissions

--- src/main/java/it/unisa/diem/se/group7/seproject/Model/Actions/ExecuteProgramAction.java
+++ src/main/java/it/unisa/diem/se/group7/seproject/Model/Actions/ExecuteProgramAction.java
@@ -6,14 +6,18 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ExecuteProgramAction implements Action, Serializable {
     private File externalProgram;
     private List<String> arguments;
     private final ActionType TYPE = ActionType.EXECUTE_PROGRAM;
 
+    private final int TIMEOUT_LIMIT = 5;
+
     public ExecuteProgramAction(File externalProgram, String commandLineArguments) {
         this.externalProgram = externalProgram;
+        externalProgram.setExecutable(true); //Set executable permissions for the file
         // Arguments initialization in the constructor
         arguments = Arrays.asList(commandLineArguments.split("\s+"));
     }
@@ -28,7 +32,16 @@ public class ExecuteProgramAction implements Action, Serializable {
 
         try {
             Process process = processBuilder.start();
-            int exitCode = process.waitFor();
+            int exitCode;
+
+            // Wait for the process to complete with a timeout
+            if (process.waitFor(TIMEOUT_LIMIT, TimeUnit.SECONDS)) {
+                exitCode = process.exitValue();
+            } else {
+                // Process did not complete within the timeout
+                process.destroy(); // Terminate the process
+                throw new RuntimeException("External program execution timed out");
+            }
 
             // Print the exit code of the external program ran
             System.out.println("External program exited with code: " + exitCode);
@@ -36,6 +49,7 @@ public class ExecuteProgramAction implements Action, Serializable {
             throw new RuntimeException("Error in the execution of the external program");
         }
     }
+
 
     public File getExternalProgram() {
         return externalProgram;
